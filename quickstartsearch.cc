2c2c52b8f3ea5eb14ffd0fbe488e3f1f82bfa5ba
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2012-05-08 00:15:04 +0300

    searcher code

--- quickstartsearch.cc
+++ quickstartsearch.cc
@@ -1,4 +1,4 @@
-/* quickstartindex.cc: Simplest possible indexer
+/* quickstartsearch.cc: Simplest possible searcher
  *
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
@@ -26,32 +26,43 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    // Simplest possible options parsing: we just require three or more
+    // Simplest possible options parsing: we just require two or more
     // parameters.
-    if(argc < 4) {
+    if (argc < 3) {
         cout << "usage: " << argv[0] <<
-                " <path to database> <document data> <document terms>" << endl;
+                " <path to database> <search terms>" << endl;
         exit(1);
     }
 
     // Catch any Xapian::Error exceptions thrown
     try {
         // Make the database
-        Xapian::WritableDatabase database(argv[1], Xapian::DB_CREATE_OR_OPEN);
+        Xapian::Database db(argv[1]);
 
-        // Make the document
-        Xapian::Document newdocument;
+        // Start an enquire session
+        Xapian::Enquire enquire(db);
 
-        // Put the data in the document
-        newdocument.set_data(string(argv[2]));
+        // Build the query object
+        Xapian::Query query(Xapian::Query::OP_OR, argv + 2, argv + argc);
+        cout << "Performing query `" << query.get_description() << "'" << endl;
 
-        // Put the terms into the document
-        for (int i = 3; i < argc; ++i) {
-            newdocument.add_posting(argv[i], i - 2);
+        // Give the query object to the enquire session
+        enquire.set_query(query);
+
+        // Get the top 10 results of the query
+        Xapian::MSet matches = enquire.get_mset(0, 10);
+
+        // Display the results
+        cout << matches.size() << " results found" << endl;
+
+        for (Xapian::MSetIterator i = matches.begin();
+             i != matches.end();
+             ++i) {
+            Xapian::Document doc = i.get_document();
+            cout << "Document ID " << *i << "\t" <<
+                    i.get_percent() << "% [" <<
+                    doc.get_data() << "]" << endl;
         }
-
-        // Add the document to the database
-        database.add_document(newdocument);
     } catch(const Xapian::Error &error) {
         cout << "Exception: "  << error.get_msg() << endl;
     }
